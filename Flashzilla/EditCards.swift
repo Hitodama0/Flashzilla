5d732dbc2278f7c51e491b6bd68881edf37d5624
Biagio <biagior00@gmail.com>
2023-02-28 16:09:42 +0100

    Project 17 with challenge 1 and 4

---
+++ Flashzilla/EditCards.swift
@@ -0,0 +1,89 @@
+//
+//  EditCards.swift
+//  Flashzilla
+//
+//  Created by Biagio Ricci on 28/02/23.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var cards = [Card]()
+    @State private var newPrompt = ""
+    @State private var newAnswer = ""
+    let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedData")
+    var body: some View {
+        NavigationView {
+            List {
+                Section("Add new card") {
+                    TextField("Prompt", text: $newPrompt)
+                    TextField("Answer", text: $newAnswer)
+                    Button("Add card", action: addCard)
+                }
+                
+                Section {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            Text(cards[index].prompt)
+                                .font(.headline)
+                            
+                            Text(cards[index].answer)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .onDelete(perform: removeCards)
+                }
+            }
+            .navigationTitle("Edit Cards")
+            .toolbar {
+                Button("Done", action: done)
+            }
+            .listStyle(.grouped)
+            .onAppear(perform: loadData)
+        }
+    }
+    
+    func done() {
+        dismiss()
+    }
+    
+    func loadData() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+                return
+            }
+        }
+    }
+    
+    func saveData() {
+        if let encoded = try? JSONEncoder().encode(cards) {
+            try? encoded.write(to: savePath, options: [.atomic, .completeFileProtection])
+        }
+    }
+    
+    func addCard() {
+        let trimmedPrompt = newPrompt.trimmingCharacters(in: .whitespaces)
+        let trimmedAnswer = newAnswer.trimmingCharacters(in: .whitespaces)
+        guard trimmedPrompt.isEmpty == false && trimmedAnswer.isEmpty == false else { return }
+        
+        newPrompt = ""
+        newAnswer = ""
+        
+        let card = Card(prompt: trimmedPrompt, answer: trimmedAnswer)
+        cards.insert(card, at: 0)
+        saveData()
+    }
+    
+    func removeCards(at offsets: IndexSet) {
+        cards.remove(atOffsets: offsets)
+        saveData()
+    }
+}
+
+struct EditCards_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCards()
+    }
+}
